6c09ef1735cbdb8e32d0417fd1a1412456d47f9a
kelleman <kellemanatser@gmail.com>
2023-05-17 18:37:20 +0100

    .

---
+++ app-tracker/client/src/components/Logo.js
@@ -0,0 +1,6 @@
+import logo from '../assets/images/logo4.png';
+
+const Logo = () => {
+  return <img src={logo} alt='Jobs-tracker' className='logo' />;
+};
+export default Logo;
---
+++ app-tracker/client/src/pages/Landing.js
@@ -0,0 +1,30 @@
+import main from '../assets/images/main.svg';
+import Wrapper from '../assets/wrappers/LandingPage';
+import { Logo } from '../components';
+import { Link } from 'react-router-dom';
+const Landing = () => {
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className='container page'>
+        {/* info */}
+        <div className='info'>
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            Welcome to our jobs tracking app where you can keep track of all your jobs applications, with this app you can as well keep track of the your application details and its status
+          </p>
+          <Link to='/register' className='btn btn-hero'>
+            Login/Register
+          </Link>
+        </div>
+        <img src={main} alt='job hunt' className='img main-img' />
+      </div>
+    </Wrapper>
+  );
+};
+
+export default Landing;
