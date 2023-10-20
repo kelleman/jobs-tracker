2ab3df515577f9f53c9c84cd98b45809676d12f6
kelleman <kellemanatser@gmail.com>
2023-10-20 17:44:42 +0100

    .

--- app-tracker/client/src/pages/Register.js
+++ app-tracker/client/src/pages/Register.js
@@ -80,7 +80,7 @@ function Register() {
         <button type='submit' className='btn btn-block' disabled={isLoading}>
           {isLoading ? 'loading...' : 'submit'}
         </button>
-        <button
+        {/* <button
           type='button'
           className='btn btn-block btn-hipster'
           disabled={isLoading}
@@ -91,7 +91,7 @@ function Register() {
           }
         >
           {isLoading ? 'loading...' : 'demo app'}
-        </button>
+        </button> */}
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
           <button type='button' onClick={toggleMember} className='member-btn'>
